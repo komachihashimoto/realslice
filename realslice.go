f590ccd677ca264008071d5af0cdb860c4976868
komachihashimoto <joujinameko@gmail.com>
2024-01-09 14:33:54 +0900

    slice

--- realslice.go
+++ realslice.go
@@ -7,4 +7,8 @@ func main(){
 	slice13 := intarray[1:4]
 	slice0 := intarray[:1]
 	slice4 := intarray[4:]
+
+	fmt.Printf("slice13[1]は%d\n",slice13[1])
+	fmt.Printf("slice0の要素数は%d\n",len(slice0))
+	fmt.Printf("slice4[0]の値は%d\n",slice4[0])
 }
